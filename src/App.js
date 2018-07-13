9e1d64d44ec3b3611fd28e5f0746c74eb23da036
bananovitch <piotr.michal.romanowski@gmail.com>
2018-07-13 18:20:23 +0200

    added basic bagination

--- src/App.js
+++ src/App.js
@@ -3,24 +3,29 @@ import './App.css';
 import fetch from 'isomorphic-fetch';
 
 const pathBase = "https://api-v3.mojepanstwo.pl/dane/poslowie.json";
+const requestType =  "_type=objects";
+const paramPage = 'page=';
+
 
 
 class App extends Component {
-
-  
 
   constructor(props) {
     super(props);
     this.fetchSpeeches = this.fetchSpeeches.bind(this);
     this.state = {
-      result:[]
+      result:[],
+      page:0
     }
   }
 
-  fetchSpeeches() {
-    fetch(pathBase)
+  fetchSpeeches( page = 0 ) {
+    fetch(`${pathBase}?${requestType}&${paramPage}${page}`)
     .then(response => response.json())
-    .then(response => this.setState({ result:response.Dataobject }));
+    .then(response => this.setState({ 
+      result:response.Dataobject,
+      page
+     }));
   }
 
   render() {
@@ -30,11 +35,20 @@ class App extends Component {
           <h1 className="App-title">Sejmator</h1>
         </header>
         <h2>Sejmator</h2>
-        <button onClick={() => this.fetchSpeeches() }>Pobierz dane</button>
-        <ol>{this.state.result.map( (item) => <li>{item.slug}</li> )}</ol>
+        <button onClick={() => this.fetchSpeeches(this.state.page) }>Pobierz dane</button>
+        <button onClick={() => this.fetchSpeeches(this.state.page - 1) } >Poprzednia strona</button>
+        <button onClick={() => this.fetchSpeeches(this.state.page + 1) }>Następna strona strona</button>
+        <ol>
+        {this.state.result.map( (item, index) => 
+          <li key={index}>{item.slug}</li> 
+          )}
+        </ol>
+        
       </div>
     );
   }
 }
+
+
 
 export default App;
